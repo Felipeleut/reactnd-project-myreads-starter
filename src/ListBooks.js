6e7f4ec7e26bbb3e3242ccac9c40e5ebca146221
Felipeleut <felipeleut@gmail.com>
2018-06-10 14:18:42 -0300

    Feat: Arquivo ListBooks.js criado
    Criado para conter os componentes da UI de listagem de livros.

---
+++ src/ListBooks.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ListBooks extends Component{
+    render(){
+        return(
+            <div>
+                Lista de Livros
+            </div>
+        )
+    }
+}
+
+export default ListBooks
